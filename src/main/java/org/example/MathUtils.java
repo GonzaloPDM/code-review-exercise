fd502fe9d707302333281b79bfa350920cc0c729
GonzaloPDM <gonzalopantindm@gmail.com>
2026-04-28 11:57:10 +0200

    feat: initial project with Calculator and MathUtils

---
+++ src/main/java/org/example/MathUtils.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Auxiliary math utilities.
+ */
+public class MathUtils {
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+    public static boolean isPositive(int number) {
+        return number > 0;
+    }
+}
+
